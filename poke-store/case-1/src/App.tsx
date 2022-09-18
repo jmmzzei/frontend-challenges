d1986c525f9a9badba825236e369c1af002ccecd
José María <josemmazzei@gmail.com>
2022-09-18 10:38:20 -0300

    Resolve poke-store/case-1

--- poke-store/case-1/src/App.tsx
+++ poke-store/case-1/src/App.tsx
@@ -1,22 +1,78 @@
-import type {Pokemon} from "./types";
+import type { Pokemon } from "./types";
 
-import {useState} from "react";
+import { useMemo, useState, useEffect } from "react";
 
-import {POKEMONS} from "./constants";
+import { POKEMONS } from "./constants";
 
 function App() {
   const [cart, setCart] = useState<Pokemon[]>([]);
+  const [filteredPokemons, setFilteredPokemons] = useState<Pokemon[]>(POKEMONS);
+  const [favorites, setFavorites] = useState<Pokemon[]>(() =>
+    JSON.parse(localStorage.getItem("fav") || "[]")
+  );
+
+  const cartTotal = useMemo(
+    () => cart.reduce((acc, curr) => acc + curr.price, 0),
+    [cart]
+  );
+
+  const handleAddToCart = (pokemon: Pokemon) => {
+    if (cartTotal + pokemon.price > 10) return;
+    setCart((cart) => cart.concat(pokemon));
+  };
+
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value: search },
+    } = e;
+
+    setFilteredPokemons(
+      POKEMONS.filter((pokemon) =>
+        pokemon.name.toLowerCase().includes(search.toLowerCase())
+      )
+    );
+  };
+
+  const isFavorite = (id: Pokemon["id"]) =>
+    favorites.find((fav) => fav.id == id);
+
+  const handleAddToFavorites = (pokemon: Pokemon) => {
+    if (isFavorite(pokemon.id)) {
+      setFavorites((favorites) =>
+        favorites.filter((favorite) => favorite.id !== pokemon.id)
+      );
+      return;
+    }
+
+    setFavorites((favorites) => favorites.concat(pokemon));
+  };
+
+  useEffect(() => {
+    localStorage.setItem("fav", JSON.stringify(favorites));
+  }, [favorites]);
 
   return (
     <>
       <nav>
-        <input className="nes-input" id="name_field" placeholder="Charmander" type="text" />
+        <input
+          className="nes-input"
+          id="name_field"
+          placeholder="Charmander"
+          type="text"
+          onChange={handleSearch}
+        />
       </nav>
       <section>
-        {POKEMONS.map((pokemon) => (
+        {filteredPokemons.map((pokemon) => (
           <article key={pokemon.id}>
-            <figure>
-              <i className="nes-icon is-medium is-transparent heart" />
+            <figure onClick={() => handleAddToFavorites(pokemon)}>
+              <i
+                className={
+                  isFavorite(pokemon.id)
+                    ? "nes-icon is-medium heart"
+                    : "nes-icon is-medium is-transparent heart"
+                }
+              />
               <img className="nes-container" src={pokemon.image} />
             </figure>
             <div>
@@ -25,14 +81,19 @@ function App() {
               </p>
               <p>{pokemon.description}</p>
             </div>
-            <button className="nes-btn" onClick={() => setCart(cart.concat(pokemon))}>
+            <button
+              className="nes-btn"
+              onClick={() => handleAddToCart(pokemon)}
+            >
               Agregar
             </button>
           </article>
         ))}
       </section>
       <aside>
-        <button className="nes-btn is-primary">0 items (total $0)</button>
+        <button className="nes-btn is-primary">
+          0 items (total ${cartTotal})
+        </button>
       </aside>
     </>
   );
