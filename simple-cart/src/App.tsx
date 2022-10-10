4fb5e74172784e4dfd9350edaf53a9b135088dbd
José María <josemmazzei@gmail.com>
2022-10-09 21:33:31 -0300

    Improve simple-cart - second step

--- simple-cart/src/App.tsx
+++ simple-cart/src/App.tsx
@@ -5,12 +5,6 @@ import { Product, CartItem } from "./types";
 
 import styles from "./index.module.scss";
 
-// * Si tenemos algún elemento del producto en el carrito debemos cambiar el botón de "Agregar" por uno que tenga un botón de "-", la cantidad de productos que tenemos en el carrito y un botón de "+", clickear los botones correspondientes va a sumar o restar elementos del carrito. Si tenía un producto en el carrito y clickeo en "-" debe eliminar el producto del carrito.
-
-// Done:
-// * Cada producto debe agregarse al carrito al clickear "Agregar"
-// * El botón inferior nos marca cuantos productos tenemos en el carrito y su total, mostrar los valores reales del carrito en todo momento.
-
 function App() {
   const [products, setProducts] = useState<Product[]>([]);
   const [cart, setCart] = useState<CartItem[]>([]);
@@ -45,47 +39,36 @@ function App() {
   const isInCart = (id: Product["id"]) =>
     Boolean(cart.find((item) => item.id === id));
 
-  const handleIncrement = (id: Product["id"]) => {
-    const productFinded = cart.find((item) => item.id === id);
-
-    if (productFinded) {
-      const cartItem = {
-        ...productFinded,
-        quantity: productFinded.quantity + 1,
-      };
-
-      setCart((products) =>
-        products.map((item) => (item.id == id ? cartItem : item))
-      );
-    }
+  const handleIncrement = (id: CartItem["id"]) => {
+    setCart((products) =>
+      products.map((item) =>
+        item.id == id
+          ? {
+              ...item,
+              quantity: item.quantity + 1,
+            }
+          : item
+      )
+    );
   };
 
-  const handleDecrement = (id: Product["id"]) => {
-    const productFinded = cart.find((item) => item.id === id);
-
-    if (productFinded && productFinded.quantity == 1) {
-      setCart((cart) => cart.filter((item) => item.id !== id));
-
-      return;
-    }
-
-    if (productFinded) {
-      const cartItem = {
-        ...productFinded,
-        quantity: productFinded.quantity - 1,
-      };
-
-      setCart((products) =>
-        products.map((item) => (item.id == id ? cartItem : item))
-      );
-    }
+  const handleDecrement = (id: CartItem["id"]) => {
+    setCart((products) =>
+      products
+        .map((item) =>
+          item.id == id
+            ? {
+                ...item,
+                quantity: item.quantity - 1,
+              }
+            : item
+        )
+        .filter((item) => item.quantity)
+    );
   };
 
-  const getItemQtty = (id: Product["id"]) => {
-    const cartFinded = cart.find((item) => item.id === id);
-
-    return cartFinded?.quantity;
-  };
+  const getItemQtty = (id: Product["id"]) =>
+    cart.find((item) => item.id === id)?.quantity;
 
   return (
     <main className={styles.main}>
@@ -100,14 +83,24 @@ function App() {
             </div>
             {isInCart(product.id) ? (
               <div>
-                <button onClick={() => handleDecrement(product.id)}>-</button>
-                <p>{getItemQtty(product.id)}</p>
-                <button onClick={() => handleIncrement(product.id)}>+</button>
+                <button
+                  className={styles.button}
+                  onClick={() => handleDecrement(product.id as CartItem["id"])}
+                >
+                  -
+                </button>
+                <p className={styles.quantity}>{getItemQtty(product.id)}</p>
+                <button
+                  className={styles.button}
+                  onClick={() => handleIncrement(product.id as CartItem["id"])}
+                >
+                  +
+                </button>
               </div>
             ) : (
               <button
-                onClick={() => handleAdd(product)}
                 className={styles.button}
+                onClick={() => handleAdd(product)}
               >
                 Agregar
               </button>
